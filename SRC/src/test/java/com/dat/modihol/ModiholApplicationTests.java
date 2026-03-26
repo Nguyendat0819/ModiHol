cc85e7ca9f6125d4642162accccd4ac95cb4d9d7
Nguyendat0819 <23010282@st.phenikaa-uni.edu.vn>
2026-03-26 07:30:18 +0000

    tạo project

---
+++ SRC/src/test/java/com/dat/modihol/ModiholApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dat.modihol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModiholApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
